731bce7b79f3335b3b7694deecdf4a985177314b
Wilfens Exantus <exantusw1@montclair.edu>
2026-03-12 22:32:06 +0000

    Spring Boot project

---
+++ src/main/java/com/example/midtermjenkins/MidtermjenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.midtermjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidtermjenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidtermjenkinsApplication.class, args);
+	}
+
+}
